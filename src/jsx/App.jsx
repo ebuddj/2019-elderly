ce8759e617d64a699e447ce6446407bb85bf26ec
Teemo Tebest <teemo.tebest@gmail.com>
2019-06-07 14:57:25 +0200

    Updatd

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -13,6 +13,15 @@ import {Polar} from 'react-chartjs-2';
 
 let data = [];
 
+const order = {
+  'Africa':3,
+  'Asia':4,
+  'Europe':0,
+  'N. America':1,
+  'Oceania':5,
+  'S. America':2
+}
+
 class App extends Component {
   constructor() {
     super();
@@ -23,6 +32,9 @@ class App extends Component {
           data:[]
         }],
         labels:[]
+      },
+      animation: {
+        duration: 500
       },
       year:1960,
       hoveredSection: false
@@ -38,8 +50,8 @@ class App extends Component {
       setInterval(() => {
         if (self.state.year >= 2017) {
           self.setState((state, props) => ({
-          year:1960
-        }), self.updateData);
+            year:1960
+          }), self.updateData);
         }
         else {
           self.setState((state, props) => ({
@@ -59,16 +71,17 @@ class App extends Component {
     let current_data = {
       datasets:[{
         backgroundColor:[],
+        borderAlign:[],
         borderColor:[],
         borderWidth:[],
-        borderAlign:[],
-        data:[]
+        data:[],
+        hoverBackgroundColor:'#000'
       }],
       labels:[]
     };
-    _.each(_.sortBy(data, (country) => { return country.continent; }), (country) => {
-      current_data.datasets[0].data.push(country[this.state.year]);
-      current_data.datasets[0].backgroundColor.push(this.getCountryColor(country.continent, country[this.state.year] * 3));
+    _.each(_.sortBy(data, (country) => { return order[country.continent]; }), (country) => {
+      current_data.datasets[0].data.push(country[this.state.year] * 100);
+      current_data.datasets[0].backgroundColor.push(this.getCountryColor(country.continent, country[this.state.year] * 4));
       current_data.datasets[0].borderWidth.push(0);
       current_data.labels.push(country.name);
     });
@@ -77,24 +90,75 @@ class App extends Component {
     }));
   }
   getCountryColor(continent, value) {
+    // rgba(230, 25, 75, 1.0)
+    // rgba(60, 180, 75, 1.0)
+    // rgba(245, 130, 49, 1.0)
+    // rgba(128, 0, 0, 1.0)
+    // rgba(240, 50, 230, 1.0)
+    // rgba(0, 0, 117, 1.0)
     let continentColors = {
-      'Africa':'rgba(255, 0, 0, ' + value + ')',
-      'Asia':'rgba(255, 127, 0, ' + value + ')',
-      'Asia':'rgba(255, 255, 0, ' + value + ')',
-      'Europe':'rgba(0, 255, 0, ' + value + ')',
-      'N. America':'rgba(0, 0, 255, ' + value + ')',
-      'Oceania':'rgba(75, 0, 130, ' + value + ')',
-      'S. America':'rgba(139, 0, 255, ' + value + ')'
+      'Africa':'rgba(230, 25, 75, ' + value + ')',
+      'Asia':'rgba(60, 180, 75, ' + value + ')',
+      'Europe':'rgba(245, 130, 49, ' + value + ')',
+      'N. America':'rgba(128, 0, 0, ' + value + ')',
+      'Oceania':'rgba(240, 50, 230, ' + value + ')',
+      'S. America':'rgba(0, 0, 117, ' + value + ')'
     }
     return continentColors[continent];
   }
   render() {
     let options = {
+      onHover:function(event) {
+        // console.log(event)
+      },
+      tooltips: {
+        enabled: false,
+        custom: function(tooltipModel) {
+          let tooltipEl = document.getElementById('chartjs-tooltip');
+          if (!tooltipEl) {
+            tooltipEl = document.createElement('div');
+            tooltipEl.id = 'chartjs-tooltip';
+            tooltipEl.innerHTML = '<div></div>';
+            document.body.appendChild(tooltipEl);
+          }
+          if (tooltipModel.opacity === 0) {
+            tooltipEl.style.opacity = 0;
+            return;
+          }
+          if (tooltipModel.yAlign) {
+            tooltipEl.classList.add(tooltipModel.yAlign);
+          }
+          else {
+            tooltipEl.classList.add('no-transform');
+          }
+          if (tooltipModel.body) {
+            tooltipEl.innerHTML = tooltipModel.body[0].lines[0].split(':')[0];
+          }
+          let position = this._chart.canvas.getBoundingClientRect();
+          tooltipEl.style.opacity = 1;
+          tooltipEl.style.position = 'absolute';
+          tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
+          tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
+          tooltipEl.style.fontFamily = tooltipModel._bodyFontFamily;
+          tooltipEl.style.fontSize = tooltipModel.bodyFontSize + 'px';
+          tooltipEl.style.fontStyle = tooltipModel._bodyFontStyle;
+          tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
+          tooltipEl.style.pointerEvents = 'none';
+        }
+      },
       responsive: true,
+      tooltipEvents: ['click'],
       scale: {
         ticks: {
+          callback:function(value) {
+            return value + '%';
+          },
+          fontSize: 11,
+          fontStyle:'normal',
+          fontFamily: 'verdana',
+          max: 20,
           min: 0,
-          max: 0.2
+          stepSize: 2
         }
       },
       legend: {
